46bad5c1f154735af29ccce20c51a367c6d6ae8d
CodyMacc <unconfigured@null.spigotmc.org>
2026-07-16 11:22:43 -0700

    Permission exercise finished

--- FCC-Bools/Program.cs
+++ FCC-Bools/Program.cs
@@ -1,4 +1,36 @@
-﻿Random coin = new Random();
+﻿//Coin Flip Exercise
+Random coin = new Random();
 int coinNum = coin.Next(100);
 string result = coinNum > 50 ? "heads" : "tails";
 Console.WriteLine(result);
+
+//Permission Exercise
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
